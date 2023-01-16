16fd87c1f8078510c4a3cb5f78443a77be2e88ea
Andi Hoerudin <andihoerudin24@gmail.com>
2023-01-16 22:56:27 +0700

    creating a login page and handler

--- goand/helpers.go
+++ goand/helpers.go
@@ -1,6 +1,24 @@
 package goand
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
+
+const (
+	randomString = "abcdefghijklmnopqrstufwxyzABCDEFGHIJKLMNOPQRSTUFWXYZ0987654321_+"
+)
+
+//Randomstring generates a random string length  n from values in the const randomstring
+func (g *Goand) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomString)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
 
 func (g *Goand) CreateDirIfNotExist(path string) error {
 	const mode = 0755
--- myapp/routes.go
+++ myapp/routes.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"myapp/data"
 	"net/http"
+	"strconv"
 )
 
 func (a *application) routes() *chi.Mux {
@@ -15,6 +16,9 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
+
+	a.App.Routes.Get("/users/login", a.Handlers.Userlogin)
+	//a.App.Routes.Post("/users/login")
 
 	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
 		u := data.User{
@@ -29,6 +33,38 @@ func (a *application) routes() *chi.Mux {
 			a.App.ErrorLog.Println(err)
 		}
 		fmt.Fprintf(w, "%d: %s", id, u.FirstName)
+	})
+
+	a.App.Routes.Get("/get-all-users", func(writer http.ResponseWriter, request *http.Request) {
+		users, err := a.Models.Users.GetAll()
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+		}
+		for _, x := range users {
+			fmt.Fprintf(writer, x.LastName)
+		}
+	})
+
+	a.App.Routes.Get("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+		u, _ := a.Models.Users.Get(id)
+		fmt.Fprintf(w, "%s %s %s", u.FirstName, u.LastName, u.Email)
+	})
+
+	a.App.Routes.Get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+		u, err := a.Models.Users.Get(id)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+		}
+
+		u.LastName = a.App.RandomString(10)
+		err = u.Update(*u)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+		fmt.Fprintf(w, "update last name to %s", u.LastName)
 	})
 
 	//static route
