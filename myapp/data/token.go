419fc37cdf43dcc4c691ca2936fffdae2df7bb83
Andi Hoerudin <andihoerudin24@gmail.com>
2023-01-24 23:27:33 +0700

    writing tests for models go

--- myapp/data/token.go
+++ myapp/data/token.go
@@ -155,12 +155,14 @@ func (t *Token) AuthenticatedToken(r *http.Request) (*User, error) {
 	if len(token) != 26 {
 		return nil, errors.New("token wrong size")
 	}
-	t, err := t.GetByToken(token)
+
+	tkn, err := t.GetByToken(token)
+
 	if err != nil {
 		return nil, errors.New("no matching token found")
 	}
 
-	if t.Expires.Before(time.Now()) {
+	if tkn.Expires.Before(time.Now()) {
 		return nil, errors.New("expired token")
 	}
 
